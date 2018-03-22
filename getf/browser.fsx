1bde1806d86d68cb265c9dcb5f48ceaa8a104cc2
Eric Lobdell <eric.lobdell@outlook.com>
2018-03-22 18:43:25 -0400

    add browser script

---
+++ getf/browser.fsx
@@ -0,0 +1,14 @@
+﻿open System
+open System.Net
+open System.Windows.Forms
+
+let browser = 
+  let webClient = new WebClient()
+  let fSharpDotOrg = webClient.DownloadString(Uri "http://fsharp.org")
+  new WebBrowser(ScriptErrorsSuppressed = true,
+                 Dock = DockStyle.Fill,
+                 DocumentText = fSharpDotOrg)
+
+let form = new Form(Text = "Hello From F#!")
+form.Controls.Add browser
+form.Show()
